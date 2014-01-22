9bd8379e2005d8c93a6b05d3919345cd9bc6df5c
Michael Reilly <mikereilly@google.com>
2014-01-22 15:54:57 -0800

    Adding ReverseInput

---
+++ Text/ReverseInput.java
@@ -0,0 +1,13 @@
+/** TODO: http://go/java-style#javadoc */
+public class ReverseInput {
+
+  public static void main(String[] args) {
+    String in = args[0];
+    char[] input = in.toCharArray();
+    System.out.println('\n');
+    for (int ind = input.length - 1; ind >= 0; ind--) {
+      System.out.print(input[ind]);
+    }
+    System.out.println('\n');
+  }
+}
